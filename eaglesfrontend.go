92443b39ceb8ee29bcd892debc41e08fa7adeb19
abdul2 <abdul.abdi@digital.cabinet-office.gov.uk>
2016-05-21 20:27:23 +0100

    hjk

--- eaglesfrontend.go
+++ eaglesfrontend.go
@@ -4,7 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
+	//"io/ioutil"
+	//"encoding/json"
+	"io/ioutil"
+
+	"encoding/json"
 )
+
+
+
+//Game describes results of football game
+type Game struct {
+
+Gamedate string `json:"gamedate"`
+Team string `json:"team"`
+Awayorhome string `json:"awayorhome"`
+Competition string `json:"competition"`
+Result string `json:"result"`
+Score1 string `json:"score1"`
+Score2 string `json:"score2"`
+}
+
+var Content []Game
 
 func main() {
 
@@ -16,6 +37,19 @@ func main() {
 }
 
 func Showresults(w http.ResponseWriter, req *http.Request)  {
+
+
+
+	b, _:= http.Get("http://localhost:9000/results")
+
+	a, _ := ioutil.ReadAll(b.Body)
+
+	r := json.Unmarshal(a,&Content)
+
+	if r != nil {}
+
+	fmt.Println(Content)
+
 
 	t, _ := template.ParseFiles("index.html")
 
